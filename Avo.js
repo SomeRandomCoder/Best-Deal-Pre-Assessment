d24669f055442659225ca85409709584e088100b
Tyron <tyron.burkhardt.za@gmail.com>
2016-03-31 10:48:35 +0200

    mapping complete

--- Avo.js
+++ Avo.js
@@ -1,8 +1,32 @@
 exports.convertion = function(str){
-  var step1 = str.split(', ').join(",").replace(/for /g, '');
+  var step1 = str.replace(/for /g, '').split(', ').join(",");
   var step2 = step1.split(' ').join('');
   var step3 = step2.replace(/R/g, " ").split(" ").join(",").split(",");
-
+var array = step3;
   console.log(step3);
   return step3;
+};
+
+exports.mapping = function(array){
+var quantity = [];
+var price =[];
+var map =[];
+
+for(var i = 0; i< array.length; i++){
+  if(i % 2 === 0){
+    quantity.push(array[i]);
+  }
+  else if(i % 2 !=0){
+    price.push(array[i]);
+  };
 }
+  for(var j = 0; j< quantity.length; j++){
+    map.push({
+      Deal: j +1,
+      quantity: Number(quantity[j]),
+      price: Number(price[j])
+    })
+  }
+console.log(map);
+return map;
+};
--- Avo_test.js
+++ Avo_test.js
@@ -8,3 +8,16 @@ describe("convertion", function(){
 );
   });
 });
+
+
+describe("mapping", function(){
+  it("should return the deals in a map within an array", function(){
+    var result = Avo.mapping([ '1', '3', '2', '7', '3', '10', '5', '14.50' ]);
+    assert.deepEqual(result,
+[ { Deal: 1, quantity: 1, price: 3 },
+  { Deal: 2, quantity: 2, price: 7 },
+  { Deal: 3, quantity: 3, price: 10 },
+  { Deal: 4, quantity: 5, price: 14.5 } ]);
+  })
+
+})
